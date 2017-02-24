5ffcc83242682b0d7815b0bdd9bd8c5728c5d69b
Renwick D'souza <renwickdaren@gmail.com>
2017-02-24 19:05:59 +0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#This is the install script for MacBuntu OS.
+#Open the README file for more details
+
+
+#!/bin/bash
+
+sudo add-apt-repository ppa:noobslab/macbuntu
+sudo apt-get update
+
+sudo apt-get install git
+git clone https://github.com/B00merang-Project/macOS-Sierra.git
+
+sudo mkdir ~/.themes
+sudo mkdir ~/.fonts
+
+sudo cp ~/MacBuntu-OS/macOS-fonts ~/.fonts
+sudo cp ~/MacBuntu-OS/macOS-Sierra ~/.themes
+
+sudo apt-get install macbuntu-os-icons-lts-v7 -y
+sudo apt-get install slingscold -y
+sudo apt-get install albert -y
+sudo apt-get install plank -y
+sudo apt-get install unity-tweak-tool -y
+
+
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#This is the uninstall script for MacBuntu OS.
+
+#!/bin/bash
+
+sudo apt-get remove macbuntu-os-icons-lts-v7 -y
+sudo apt-get remove slingscold -y
+sudo apt-get remove albert -y
+sudo apt-get remove plank -y
